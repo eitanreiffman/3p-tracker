871099351e06cd0b7ef484c490ea5b5636eaa2c7
Eitan Reiffman <eitanreiffman@gmail.com>
2023-06-11 10:26:42 -0400

    first commit

---
+++ src/components/MediumBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MediumBox.css'
+
+export default function MediumBox() {
+  return (
+    <div className='medium-box'>
+        <span>Duncan Robinson</span><br />
+        <span className='number'>8</span>
+    </div>
+  )
+}
