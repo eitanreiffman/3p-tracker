00e261b08d4ffeafeb7412e2cb5e8db2e0f101c0
Eitan Reiffman <eitanreiffman@gmail.com>
2023-06-11 11:11:05 -0400

    Added 3pt Tracker

--- src/components/MediumBox.jsx
+++ src/components/MediumBox.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './MediumBox.css'
 
 export default function MediumBox() {
+
+   const [players, setPlayers] = useState({
+    robinson: ['Duncan Robinson', 8],
+    murray: ['Jamal Murray', 10],
+    jokic: ['Nikola Jokic', 7],
+    brown: ['Bruce Brown', 7],
+    vincent: ['Gabe Vincent', 10],
+    martin: ['Caleb Martin', 5],
+    strus: ['Max Strus', 5],
+    lowry: ['Kyle Lowry', 7],
+    love: ['Kevin Love', 7],
+    gordon: ['Aaron Gordon', 5],
+    butler: ['Jimmy Butler', 5]
+   });
+
+   const updateNumber = (playerKey) => {
+    setPlayers(prevPlayers => {
+        const updatedPlayers = { ...prevPlayers };
+        updatedPlayers[playerKey][1] += 1;
+        return updatedPlayers;
+    })
+   }
+
   return (
-    <div className='medium-box'>
-        <span>Duncan Robinson</span><br />
-        <span className='number'>8</span>
-    </div>
+    <>
+    {Object.entries(players).map(([key, value]) => (
+            <div className='medium-box' key={key} onClick={() => updateNumber(key)}>
+                <div>{value[0]}</div>
+                <div>{value[1]}</div>
+            </div>
+        )
+      )}
+    </>
   )
 }
